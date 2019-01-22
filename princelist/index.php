508656382a75bb7d557fe8f721de4a323e99e9a0
zl-max <34733288+zl-max@users.noreply.github.com>
2019-01-22 16:19:23 +0800

    省市地区三级联动

---
+++ princelist/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>
+	</title>
+</head>
+<body>
+	<select id="province" onchange="showPro(this)">
+		<option>请选择省份</option>
+	</select>
+	<select id="city" onchange="showCity(this)">
+		<option>请选择城市</option>
+	</select>
+	<select id="area">
+		<option>请选择地区</option>
+	</select>
+	<script type="text/javascript" src="./index.js"></script>
+</body>
+</html>
