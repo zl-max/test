9b944ca0e8a2d85df8d833b24d2e1f454f4d416e
zl-max <34733288+zl-max@users.noreply.github.com>
2019-01-23 11:29:02 +0800

    ajax实现

--- princelist/method_two/areaDo.php
+++ princelist/method_two/areaDo.php
@@ -1,12 +1,17 @@
 <?php 
-	
-/*$dsn='mysql:host=localhost;dbname=china_area';
+$dsn='mysql:host=localhost;dbname=china_area';
 $dbuser='root';
 $dbpwd='root';
 try {
 	$conn=new PDO($dsn,$dbuser,$dbpwd);
 } catch (PDOException $e) {
 	exit('Could not connect the database:<br>'.$e);
-}*/
+}
+$sql="select id,name from area where parent_id=".$_POST["parent_id"];
+$result=$conn->query($sql);
+$data=$result->fetchAll();
+// print_r($data);
+echo json_encode($data);
+$conn=null;
 
-echo $_POST['parent_id'];
+
--- princelist/method_two/index.js
+++ princelist/method_two/index.js
@@ -1,8 +1,12 @@
 // ajax
 var xmlHttp;
-
-function select(id)
+var provinceTag=document.getElementById('province');
+var cityTag=document.getElementById('city');
+var areaTag=document.getElementById('area');
+var pub_parent_type;
+function select(id,parent_type)
 {
+	pub_parent_type=parent_type;
 	xmlHttp=getXMLHttpObject();
 	if(xmlHttp==null){
 		alert("Error:Don't Support the HTTP!");
@@ -21,7 +25,7 @@ function select(id)
 	var url="areaDo.php";
 	var msg="parent_id="+id+"&rand="+Math.random();
 	xmlHttp.onreadystatechange=statechanged;   // 执行成功需要操作
-	xmlHttp.open("POST",url,true);
+	xmlHttp.open("POST",url,false);
 	xmlHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	xmlHttp.send(msg);
 
@@ -46,8 +50,26 @@ function getXMLHttpObject(){
 // ajax返回数据操作
 function statechanged(){
 	if(xmlHttp.readyState==4 && xmlHttp.status==200){
-		console.log(xmlHttp.responseText);
+		var object=JSON.parse(xmlHttp.responseText);
+		for (var i = 0; i < object.length; i++) {
+			var tag=pub_parent_type=='province'?provinceTag:(pub_parent_type=='city'?cityTag:areaTag);
+			tag.add(new Option(object[i].name,object[i].id));
+
+		}		
 	}
 }
 
-window.onload=select(0);
+function provinceShow(th){
+	cityTag.options.length=1; //初始化
+	areaTag.options.length=1;  //初始化
+	select(th.value,'city');
+}
+
+function cityShow(th){
+	areaTag.options.length=1;  //初始化
+	select(th.value,'area');
+}
+
+window.onload=select(0,'province');
+
+
